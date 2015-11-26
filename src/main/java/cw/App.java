1809716b9c434c46a9dba6bfd9dbbdc5d499131e
tedigc <tedigc@gmail.com>
2015-11-26 17:29:28 +0000

    Tweaked knn

--- src/main/java/cw/App.java
+++ src/main/java/cw/App.java
@@ -27,25 +27,21 @@ public class App {
 			e.printStackTrace();
 		}
     	
-    	// Crop every image
-    	for(String groupName : groupedImages.getGroups()) {
-    		ListDataset<FImage> groupData = groupedImages.getInstances(groupName);
-    		for(int i=0; i<groupData.size(); i++) {
-    			FImage img = cropCentre(groupData.get(i));
-    		}
-    	}
-    		
     	// Split into training and test data
     	GroupedRandomSplitter<String, FImage> splits = new GroupedRandomSplitter<String, FImage>(groupedImages, 45, 0, 5);
     	
     	// Define variables for training and test data
-    	int nTraining = splits.getTrainingDataset().numInstances();
-    	int nTest     = splits.getTestDataset().numInstances();
     	GroupedDataset<String, ListDataset<FImage>, FImage> training = splits.getTrainingDataset();
     	GroupedDataset<String, ListDataset<FImage>, FImage> test = splits.getTestDataset();
+    	int nTraining = training.numInstances();
+    	int nTest     = test.numInstances();
     	
     	
-    	// For every instance of data, within every group, turn the img into a feature vector and record it's classification
+    	System.out.println("nTraining: " + nTraining);
+    	System.out.println("nTest    : " + nTest);
+    	
+    	// TRAINING DATA
+    	// - For every instance of data, within every group, turn the img into a feature vector and record it's classification
     	int idx = 0;
     	float[][] trData = new float[nTraining][16*16];
     	String[] trClass = new String[nTraining];
@@ -53,26 +49,51 @@ public class App {
     		ListDataset<FImage> groupInstances = training.get(groupName);
     		
     		for(int i=0; i<groupInstances.size(); i++) {
-    			float[] vectorInstance = imageToFloatVector(groupInstances.get(i));
+    			float[] vectorInstance = imageToFloatVector(cropCentre(groupInstances.get(i)));
     			trData[i] = vectorInstance;
     			trClass[i] = groupName;
     			idx++;
     		}
     	}
     	
+    	for(int i=0; i<nTraining; i++) {
+    		for(int j=0; j<256; j++) {
+    			System.out.print(trData[i][j] + ", ");
+    		}
+    		System.out.println();
+    	}
     	
-		
+    	// TEST DATA
+    	// - For every instance of data, within every group, turn the img into a feature vector and record it's classification
+    	idx = 0;
+    	float[][] tsData = new float[nTest][16*16];
+    	String[] tsClass = new String[nTest];
+    	for(String groupName : test.getGroups()) {
+    		ListDataset<FImage> groupInstances = test.get(groupName);
+    		
+    		for(int i=0; i<groupInstances.size(); i++) {
+    			float[] vectorInstance = imageToFloatVector(cropCentre(groupInstances.get(i)));
+    			tsData[i] = vectorInstance;
+    			tsClass[i] = groupName;
+    			idx++;
+    		}
+    	}
+    	
+		// Define a NN object.
 		int k = 3;
 		final FloatNearestNeighboursExact nn = new FloatNearestNeighboursExact(trData);
     	    	
-		// Do KNN on tstData
-    	for(String groupName : testData.getGroups()) {
-    		ListDataset<FImage> groupData = testData.getInstances(groupName);
-    		for(int i=0; i<groupData.size(); i++) {
-    			float[] tstVector = imageToFloatVector(cropCentre(groupData.get(i)));
-    			List<IntFloatPair> neighbors = nn.searchKNN(tstVector, k);
-
+		// For every instance of test data within every group, find the KNN from the training data and it's average class.
+    	for(int i=0; i<tsData.length-1; i++) {
+    		List<IntFloatPair> neighbors = nn.searchKNN(tsData[i], k);
+    		for(IntFloatPair pair : neighbors) {
+    			System.out.println(pair);
+    			String predictedClass = trClass[pair.getFirst()];
+    			String actualClass = tsClass[i];
+    			System.out.println(predictedClass);
+    			System.out.println(actualClass);
     		}
+    		System.out.println();
     	}
     }
 	
@@ -93,7 +114,7 @@ public class App {
 	public static float[] imageToFloatVector(FImage img) {
 		
 		float[] floatVector = null;
-		for(int i=0; i< img.width; i++){
+		for(int i=0; i< img.height; i++){
 			floatVector = ArrayUtils.addAll(floatVector, img.pixels[i]);
 		}
 		return floatVector;
