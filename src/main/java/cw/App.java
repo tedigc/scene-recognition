51457f35607b55dc20680632d9490698054938a6
tedigc <tedigc@gmail.com>
2015-12-04 14:33:38 +0000

    Finished Run 2 and Run3 v1

--- src/main/java/cw/App.java
+++ src/main/java/cw/App.java
@@ -1,131 +1,13 @@
 package cw;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map.Entry;
-
-import org.apache.commons.lang.ArrayUtils;
-import org.apache.commons.vfs2.FileSystemException;
-import org.openimaj.data.dataset.GroupedDataset;
-import org.openimaj.data.dataset.ListDataset;
-import org.openimaj.data.dataset.VFSGroupDataset;
-import org.openimaj.data.dataset.VFSListDataset;
-import org.openimaj.experiment.dataset.split.GroupedRandomSplitter;
-import org.openimaj.image.DisplayUtilities;
-import org.openimaj.image.FImage;
-import org.openimaj.image.ImageUtilities;
-import org.openimaj.image.processing.resize.ResizeProcessor;
-import org.openimaj.knn.FloatNearestNeighboursExact;
-import org.openimaj.util.pair.IntFloatPair;
-
 
 public class App {
 
 
-	public static void main( String[] args ) {
-
-    	VFSGroupDataset<FImage> groupedImages = null;
-    	try {
-			groupedImages = new VFSGroupDataset<FImage>("/Users/tedigc/Desktop/training", ImageUtilities.FIMAGE_READER);
-		} catch (FileSystemException e) {
-			e.printStackTrace();
-		}
-    	
-    	// Split into training and test data
-    	GroupedRandomSplitter<String, FImage> splits = new GroupedRandomSplitter<String, FImage>(groupedImages, 45, 0, 5);
-    	
-    	// Define variables for training and test data
-    	GroupedDataset<String, ListDataset<FImage>, FImage> training = splits.getTrainingDataset();
-    	GroupedDataset<String, ListDataset<FImage>, FImage> test = splits.getTestDataset();
-    	int nTraining = training.numInstances();
-    	int nTest     = test.numInstances();
-    	
-    	
-    	System.out.println("nTraining: " + nTraining);
-    	System.out.println("nTest    : " + nTest);
-    	
-    	for(String groupName : test.getGroups()) {
-    		for(FImage img : test.get(groupName)) {
-    			DisplayUtilities.display(img);
-    		}
-    	}
-    	
-    	// TRAINING DATA
-    	// - For every instance of data, within every group, turn the img into a feature vector and record it's classification
-    	int idx = 0;
-    	float[][] trData = new float[nTraining][16*16];
-    	String[] trClass = new String[nTraining];
-    	for(String groupName : training.getGroups()) {
-    		ListDataset<FImage> groupInstances = training.get(groupName);
-    		
-    		for(int i=0; i<groupInstances.size(); i++) {
-    			float[] vectorInstance = imageToFloatVector(cropCentre(groupInstances.get(i)));
-    			trData[idx] = vectorInstance;
-    			trClass[idx] = groupName;
-    			idx++;
-    		}
-    	}
-    	
-    	// TEST DATA
-    	// - For every instance of data, within every group, turn the img into a feature vector and record it's classification
-    	idx = 0;
-    	float[][] tsData = new float[nTest][16*16];
-    	String[] tsClass = new String[nTest];
-    	for(String groupName : test.getGroups()) {
-    		ListDataset<FImage> groupInstances = test.get(groupName);
-    		
-    		for(int i=0; i<groupInstances.size(); i++) {
-    			float[] vectorInstance = imageToFloatVector(cropCentre(groupInstances.get(i)));
-    			tsData[idx] = vectorInstance;
-    			tsClass[idx] = groupName;
-    			idx++;
-    		}
-    	}
-    	
-		// Define a NN object.
-		int k = 3;
-		final FloatNearestNeighboursExact nn = new FloatNearestNeighboursExact(trData);
-    	    	
-		// For every instance of test data within every group, find the KNN from the training data and it's average class.
-    	int correct   = 0;
-    	int incorrect = 0;
-		for(int i=0; i<tsData.length-1; i++) {
-    		List<IntFloatPair> neighbors = nn.searchKNN(tsData[i], k);
-    		for(IntFloatPair pair : neighbors) {
-    			String predictedClass = trClass[pair.getFirst()];
-    			String actualClass = tsClass[i];
-    			if(predictedClass == actualClass) 
-    				correct++;
-    			else
-    				incorrect++;
-    		}
-    	}
-		double accuracy = (double) correct / (double) (correct + incorrect);
-		System.out.println("Accuracy: " + accuracy);
-    }
-	
-	
-	public static FImage cropCentre(FImage original){
+	public static void main(String[] args) {
 		
-		int resolution = 0;
-		
-		if(original.width < original.height)
-			resolution = original.width;
-		else
-			resolution = original.height;
-		
-		return ResizeProcessor.resample(original.extractCenter(resolution, resolution), 16, 16);
+		Run1 run1 = new Run1();
+		run1.run();
 	}
-	
-	
-	public static float[] imageToFloatVector(FImage img) {
-		
-		float[] floatVector = null;
-		for(int i=0; i< img.height; i++){
-			floatVector = ArrayUtils.addAll(floatVector, img.pixels[i]);
-		}
-		return floatVector;
-	}
-
 
 }
---
+++ src/main/java/cw/Run1.java
@@ -0,0 +1,156 @@
+package cw;
+
+import java.util.List;
+
+import org.apache.commons.lang.ArrayUtils;
+import org.apache.commons.vfs2.FileSystemException;
+import org.openimaj.data.dataset.GroupedDataset;
+import org.openimaj.data.dataset.ListDataset;
+import org.openimaj.data.dataset.VFSGroupDataset;
+import org.openimaj.experiment.dataset.split.GroupedRandomSplitter;
+import org.openimaj.image.FImage;
+import org.openimaj.image.ImageUtilities;
+import org.openimaj.image.processing.resize.ResizeProcessor;
+import org.openimaj.knn.FloatNearestNeighboursExact;
+import org.openimaj.util.pair.IntFloatPair;
+
+
+/*
+ *  - Run1: K-Nearest Neighbors
+ *  
+ *  
+ */
+
+
+public class Run1 {
+	
+	
+	private int K = 3;
+	private final int resolution = 16;
+	private final int imgSize = resolution * resolution;
+	
+	
+	public void run() {
+
+		// Load dataset
+    	VFSGroupDataset<FImage> groupedImages = null;
+    	try {
+			groupedImages = new VFSGroupDataset<FImage>("/Users/tedigc/Documents/University/Computer Vision/Scene Recognition/SceneRecognition/training", ImageUtilities.FIMAGE_READER);
+		} catch (FileSystemException e) {
+			e.printStackTrace();
+		}
+    	
+    	// Split into training and test data
+    	// TODO replace 90 and 10 magic numbers with variable
+    	GroupedRandomSplitter<String, FImage> splits = new GroupedRandomSplitter<String, FImage>(groupedImages, 90, 0, 10);
+    	
+    	// Define variables for training and test datasets
+    	GroupedDataset<String, ListDataset<FImage>, FImage> training = splits.getTrainingDataset();
+    	GroupedDataset<String, ListDataset<FImage>, FImage> test = splits.getTestDataset();
+    	int nTraining = training.numInstances();
+    	int nTest     = test.numInstances();
+    	  	
+    	System.out.println("nTraining: " + nTraining);
+    	System.out.println("nTest    : " + nTest);
+    	
+    	/************* TRAINING DATA *************/
+    	
+    	float[][] trData = new float[nTraining][imgSize];
+    	String[] trClass = new String[nTraining];
+    	
+    	int idx = 0;
+    	
+    	// For every instance of data, within every group, turn the img into a feature vector and record its classification
+    	for(String groupName : training.getGroups()) {
+    		
+    		// Current scene
+    		ListDataset<FImage> groupInstances = training.get(groupName);
+    		
+    		// Store vector of each image's pixels in an array
+    		for(int i = 0; i < groupInstances.size(); i++) {
+    			trData[idx] = imageToFloatVector(cropCentre(groupInstances.get(i)));
+    			trClass[idx] = groupName;
+    			idx++;
+    		}
+    	}
+    	
+    	/************** TEST DATA **************/
+    	
+    	float[][] tsData = new float[nTest][imgSize];
+    	String[] tsClass = new String[nTest];
+    	
+    	idx = 0;
+    	
+    	// For every instance of data, within every group, turn the img into a feature vector and record it's classification
+    	for(String groupName : test.getGroups()) {
+    		
+    		// Current scene
+    		ListDataset<FImage> groupInstances = test.get(groupName);
+    		
+    		// Store vector of each image's pixels in an array
+    		for(int i=0; i<groupInstances.size(); i++) {
+    			tsData[idx] = imageToFloatVector(cropCentre(groupInstances.get(i)));
+    			tsClass[idx] = groupName;
+    			idx++;
+    		}	
+    	}
+    	
+    	/************ KNN ClASSIFIER ************/
+    	
+		// Define a NN object
+		final FloatNearestNeighboursExact nn = new FloatNearestNeighboursExact(trData);
+    	    	
+    	int correct   = 0;
+    	int incorrect = 0;
+    	
+    	// For every instance of test data within every group, find the KNN from the training data and its average class.
+		for(int i = 0; i < tsData.length-1; i++) {
+			
+			// Get ordered list of pairs containing the index and distance of each neighbour
+    		List<IntFloatPair> neighbours = nn.searchKNN(tsData[i], K);
+    		
+    		// Evaluate if prediction matches the actual class
+    		for(IntFloatPair pair : neighbours) {
+    			
+    			String predictedClass = trClass[pair.getFirst()];
+    			String actualClass = tsClass[i];
+    			
+    			if(predictedClass == actualClass) 
+    				correct++;
+    			else
+    				incorrect++;	
+    		}
+    	}
+		
+		// Print evaluated accuracy
+		double accuracy = (double) correct / (double) (correct + incorrect);
+		System.out.println("Accuracy : " + accuracy);
+    }
+	
+	// Crop image to a square about the centre and resize
+	private FImage cropCentre(FImage original){
+		
+		int resolution = 0;
+		
+		if(original.width < original.height)
+			resolution = original.width;
+		else
+			resolution = original.height;
+		
+		FImage img = ResizeProcessor.resample(original.extractCenter(resolution, resolution), this.resolution, this.resolution);
+		return img.normalise();
+	}
+	
+	// Pack image pixels into a vector
+	private float[] imageToFloatVector(FImage img) {
+		
+		float[] floatVector = null;
+		
+		for(int i=0; i< img.height; i++){
+			floatVector = ArrayUtils.addAll(floatVector, img.pixels[i]);
+		}
+		return floatVector;
+	}
+
+
+}
